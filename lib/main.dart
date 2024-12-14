0eb3205e28465fe0116b08c2829610fdbf6a1b62
Yaramegahed <yaramohamed1886757@icloud.com>
2024-12-14 02:45:00 +0200

    counter

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:nti/tasks/task2.dart';
 import 'package:nti/tasks/tasks.dart';
 
 
@@ -19,7 +20,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home:  Tasks(),
+      home:  Task2(),
     );
   }
 }
--- lib/tasks/task2.dart
+++ lib/tasks/task2.dart
@@ -9,26 +9,77 @@ class Task2 extends StatefulWidget {
 }
 
 class _Task2State extends State<Task2> {
+  int counter = 0;
+
   @override
   Widget build(BuildContext context) {
+    final List<Map<String, dynamic>> data = [
+      {
+        "icon": Icons.add,
+        "action": () => setState(() {
+              counter++;
+            })
+      },
+      {
+        "icon": Icons.minimize,
+        "action": () => counter == 0
+            ? null
+            : setState(() {
+                counter--;
+              })
+      },
+      {
+        "icon": Icons.refresh,
+        "action": () => setState(() {
+              counter = 0;
+            })
+      },
+    ];
     return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        backgroundColor: Colors.greenAccent,
+        title: const Text(
+          "Counter",
+          style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+        ),
+      ),
       body: Column(
         children: [
-
+          const SizedBox(
+            height: 300,
+          ),
+          Text(
+            counter.toString(),
+            style: const TextStyle(
+                fontWeight: FontWeight.bold, fontSize: 25, color: Colors.red),
+          ),
+          Expanded(
+              child: ListView.separated(
+                  itemBuilder: (context, index) => Numbers(
+                        icon: data[index]["icon"],
+                        onPressed: data[index]["action"],
+                      ),
+                  separatorBuilder: (context, index) => const SizedBox(
+                        height: 20,
+                      ),
+                  itemCount: data.length))
         ],
       ),
     );
   }
 }
-class Numbers{
-  int x=0;
-  Function add;
-  Function min;
-  Function reverse;
-  Numbers({required this.add,required this.min,required this.reverse});
-  printer(){
-    print(x);
+
+class Numbers extends StatelessWidget {
+  const Numbers({super.key, this.onPressed, required this.icon});
+
+  final void Function()? onPressed;
+  final IconData icon;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [IconButton(onPressed: onPressed, icon: Icon(icon))],
+    );
   }
-
-
 }
